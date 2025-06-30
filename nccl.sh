5b436a4485e8804b68523d5b84d65718a26d08b3
Xinghong He <xinghong.he@oracle.com>
2025-06-30 00:33:19 -0400

    Changed to use mpirun

--- nccl.sh
+++ nccl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # number of times to run the nccl test to stress the GPUs and RDMA network. This is different from -n iterations parameter of nccl allreduce which is set below using $iter
 max=$1
@@ -15,6 +14,7 @@ fi
 
 #ORDEREDMACHINEFILE="ordered_hostfile_system_name"
 #ORDEREDRANKMACHINEFILE="rankfile_system_name"
+hostname
 echo INPUTFILE
 cat $hostfile
 
@@ -34,11 +34,11 @@ cat $hostfile
 #echo ORDEREDRANKMACHINEFILE
 #cat $ORDEREDRANKMACHINEFILE
 
-# The number of GPUs to use for the test.  Has to be multiplier of 8.  If not passed, all GPUs will be used. 
+# The number of GPUs to use for the test.  Has to be multiplier of $SLURM_GPUS_PER_NODE.  If not passed, all GPUs will be used. 
 if [ -n "$3" ]; then
   np=$3
 else
-  np=$((`less $hostfile | wc -l` * 8 ))
+  np=$((`less $hostfile | wc -l` * SLURM_GPUS_PER_NODE ))
 fi
 
 #logfile="nccl_run_allreduce.sh.log"
@@ -59,9 +59,12 @@ do
 
   first_node=`head $hostfile -n 1`
   shape=`ssh $first_node 'curl -sH "Authorization: Bearer Oracle" -L http://169.254.169.254/opc/v2/instance/shape'`
+  NCCL_NET=IB
+  NCCL_SOCKET_IFNAME=eth0
+  size_opts="-b1G -e10G -i$((1024*1024*1024*9)) -n 10"
   case $shape in
     BM.GPU.H100.8)
-      var_UCX_NET_DEVICES=mlx5_2:1
+      var_UCX_NET_DEVICES=eth0
       var_NCCL_IB_HCA="=mlx5_0,mlx5_1,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_9,mlx5_10,mlx5_12,mlx5_13,mlx5_14,mlx5_15,mlx5_16,mlx5_17"
       ;;
     BM.GPU.H200.8)
@@ -69,18 +72,19 @@ do
       var_NCCL_IB_HCA="=mlx5_0,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_9,mlx5_10,mlx5_11"
       ;;
     BM.GPU4.8)
-      var_UCX_NET_DEVICES=mlx5_4:1
+      var_UCX_NET_DEVICES=eth0
       var_NCCL_IB_HCA="=mlx5_0,mlx5_2,mlx5_6,mlx5_8,mlx5_10,mlx5_12,mlx5_14,mlx5_16,mlx5_1,mlx5_3,mlx5_7,mlx5_9,mlx5_11,mlx5_13,mlx5_15,mlx5_17"
       ;;
     BM.GPU.B4.8 | BM.GPU.A100-v2.8)
-      var_UCX_NET_DEVICES=mlx5_0:1
+      var_UCX_NET_DEVICES=eth0
       var_NCCL_IB_HCA="=mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_14,mlx5_15,mlx5_16,mlx5_17,mlx5_9,mlx5_10,mlx5_11,mlx5_12"
       ;;
     BM.GPU.A10.4 | VM.GPU.A10.?)
       var_UCX_NET_DEVICES=ens3
       var_NCCL_IB_HCA=""
-      export NCCL_NET=Socket
-      export NCCL_SOCKET_IFNAME=ens3
+      NCCL_NET=Socket
+      NCCL_SOCKET_IFNAME=ens3
+      size_opts="-b4M -e256M -i$((1024*1024*16)) -n 10"
       ;;
     *)
       echo "Shape $shape not enabled yet - add entry in $0 and re-run"
@@ -88,28 +92,28 @@ do
       ;;
   esac
 
-  export UCX_TLS=ud,self,sm,tcp
-  export UCX_NET_DEVICES="${var_UCX_NET_DEVICES},eth0"
-  export NCCL_IB_HCA="${var_NCCL_IB_HCA}"
-  export NCCL_IGNORE_CPU_AFFINITY=1
-  export NCCL_DEBUG=WARN
-  export OMPI_MCA_coll="^hcoll"
-  export OMPI_MCA_pml=ucx
-  export NCCL_IB_SL=0
-  export NCCL_IB_TC=41
-  export NCCL_IB_GID_INDEX=3
-
-  case $shape in
-    ?M.GPU.A10.?)
-      export UCX_TLS=self,sm,tcp
-      export UCX_NET_DEVICES="${var_UCX_NET_DEVICES}"
-      srun -K --time=0:40 --mpi=pmix /opt/oci-hpc/nccl-test/build/all_reduce_perf -b4M -e256M -i$((     1024*1024*16)) -n 10
-      echo "srun_rc = $?"
-      ;;
-    *)
-      srun -K --time=0:40 --mpi=pmix /opt/oci-hpc/nccl-test/build/all_reduce_perf -b1G -e10G -i$((1024*1024*1024*9)) -n 10
-      echo "srun_rc = $?"
-      ;;
-  esac  
+  timeout --preserve-status --foreground 40s mpirun --mca pml ucx \
+  --mca coll ^hcoll \
+  --bind-to none \
+  -npernode $SLURM_GPUS_PER_NODE \
+  -x HCOLL_ENABLE_MCAST_ALL=0 \
+  -x coll_hcoll_enable=0 \
+  -x UCX_TLS=self,sm,tcp \
+  -x UCX_NET_DEVICES=$var_UCX_NET_DEVICES \
+  -x RX_QUEUE_LEN=8192 \
+  -x IB_RX_QUEUE_LEN=8192 \
+  -x NCCL_DEBUG=WARN \
+  -x NCCL_IB_HCA="${var_NCCL_IB_HCA}" \
+  -x NCCL_IGNORE_CPU_AFFINITY=1 \
+  -x NCCL_IB_SL=0 \
+  -x NCCL_IB_TC=41 \
+  -x NCCL_IB_GID_INDEX=3 \
+  -x NCCL_NET=$NCCL_NET \
+  -x NCCL_SOCKET_IFNAME=$NCCL_SOCKET_IFNAME \
+  --timeout 40 \
+  --np $np --hostfile $hostfile /opt/oci-hpc/nccl-test/build/all_reduce_perf $size_opts
+  mpirun_rc=$?
 
 done
+echo "mpirun_rc = $mpirun_rc"
+exit $mpirun_rc
